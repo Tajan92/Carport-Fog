4fc460e72ff75a8d2ad8a87e1573b03d89a549b2
AndreasJensen1997 <planeten@live.dk>
2026-05-15 13:12:55 +0200

    created RoofConverter and setup up two methods. convertRooftoDto() & converterRoofDtoToEntity()

---
+++ src/main/java/app/services/converters/RoofConverter.java
@@ -0,0 +1,21 @@
+package app.services.converters;
+
+import app.dto.requestDTO.RoofRequestDTO;
+import app.dto.responseDTO.CustomerResponseDTO;
+import app.dto.responseDTO.RoofResponseDTO;
+import app.entities.Customer;
+import app.entities.Roof;
+
+public class RoofConverter {
+
+    public RoofResponseDTO convertRoofToDto() {
+
+    }
+
+
+    public Roof convertRoofDTOtoEntity(){
+
+    }
+
+
+}
