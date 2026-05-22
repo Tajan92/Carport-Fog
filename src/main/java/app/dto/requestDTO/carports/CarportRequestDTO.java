58329b9d47a8070f23593f811d08985c790b7852
MortenHelander <morten.helander@hotmail.com>
2026-05-22 12:15:20 +0200

    CarportRequest changes

--- src/main/java/app/dto/requestDTO/carports/CarportRequestDTO.java
+++ src/main/java/app/dto/requestDTO/carports/CarportRequestDTO.java
@@ -2,9 +2,11 @@ package app.dto.requestDTO.carports;
 import app.dto.requestDTO.RoofRequestDTO;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 @AllArgsConstructor
 @Getter
+@Setter
 public abstract class CarportRequestDTO {
     private double width;
     private double height;
