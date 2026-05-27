62ee2250e2ad4978fc50c4d477ac433ec14b15fb
AndreasJensen1997 <planeten@live.dk>
2026-05-27 15:49:11 +0200

    finished feature where a salesrep can see a list of all customer on their my page

--- src/main/java/app/controllers/SalesRepController.java
+++ src/main/java/app/controllers/SalesRepController.java
@@ -9,6 +9,7 @@ public class SalesRepController {
         app.get("/loginSalesRep", ctx -> ctx.render("admin-login.html"));
         app.get("/carportMakerSalesRep", ctx -> ctx.render("admin-carport-maker.html"));
         app.get("/adminProfilePage", ctx -> ctx.render("admin-my-page.html")); /* Test route lavet af AJ*/
+        app.get("/getCustomerAdmin", ctx -> ctx.render("admin-customer-details.html")); /* Test route lavet af AJ*/
 
 //        app.post("/loginSalesRep", ctx -> loginSalesRep(ctx, serviceFactory));
     }
