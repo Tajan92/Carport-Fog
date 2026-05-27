1cf3a2c28a7d1e6f91389c7a598b68dc8383bb97
AndreasJensen1997 <planeten@live.dk>
2026-05-27 15:32:17 +0200

    added customer category in admin my page, to see all customers

--- src/main/resources/public/javascript/admin-my-page.js
+++ src/main/resources/public/javascript/admin-my-page.js
@@ -1,17 +1,29 @@
+const customerCategory = document.querySelector("#my-page-category-customers")
 const inquiryCategory = document.querySelector("#my-page-category-inquiry")
 const quoteCategory = document.querySelector("#my-page-category-quote")
 const orderCategory = document.querySelector("#my-page-category-order")
 
-
+const customerResults = document.querySelector(".my-page-customer-results")
 const inquiryResults = document.querySelector(".my-page-inquiry-results")
 const quoteResults = document.querySelector(".my-page-quote-results")
 const orderResults = document.querySelector(".my-page-order-results")
 
 
+
+customerCategory.addEventListener("click", () => {
+
+    customerResults.classList.remove("hidden")
+
+    inquiryResults.classList.add("hidden")
+    quoteResults.classList.add("hidden")
+    orderResults.classList.add("hidden")
+})
+
 inquiryCategory.addEventListener("click", () => {
 
     inquiryResults.classList.remove("hidden")
 
+    customerResults.classList.add("hidden")
     quoteResults.classList.add("hidden")
     orderResults.classList.add("hidden")
 })
@@ -20,6 +32,7 @@ quoteCategory.addEventListener("click", () => {
 
     quoteResults.classList.remove("hidden")
 
+    customerResults.classList.add("hidden")
     inquiryResults.classList.add("hidden")
     orderResults.classList.add("hidden")
 })
@@ -28,6 +41,7 @@ orderCategory.addEventListener("click", () => {
 
     orderResults.classList.remove("hidden")
 
+    customerResults.classList.add("hidden")
     inquiryResults.classList.add("hidden")
     quoteResults.classList.add("hidden")
 
