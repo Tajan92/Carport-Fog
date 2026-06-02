d25a4ca586d612a8a4feb58d39871aaec63b1a3b
Theis <theisrud@gmail.com>
2026-06-02 17:15:23 +0200

    deployment

--- src/main/java/app/services/utils/GmailEmailSender.java
+++ src/main/java/app/services/utils/GmailEmailSender.java
@@ -27,6 +27,7 @@ public class GmailEmailSender {
         props.put("mail.smtp.starttls.enable", "true"); // TLS
         props.put("mail.smtp.host", "smtp.gmail.com");
         props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
 
         Session session = Session.getInstance(props, new Authenticator() {
             @Override
